1edbf86babc9055c22a7cc6f709211e77892afb6
yuiseki <yuiseki@gmail.com>
2024-10-14 00:01:04 +0900

    fix for tsc

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import React, { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
