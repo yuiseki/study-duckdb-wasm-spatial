d0b5cc30416198cf1663fe1ff93cc9fc857e7e1a
yuiseki <yuiseki@gmail.com>
2024-10-13 16:09:26 +0900

    Add CountryByPopulation

--- src/App.tsx
+++ src/App.tsx
@@ -127,17 +127,17 @@ const LargestCountries: React.FC<{ db: duckdb.AsyncDuckDB }> = ({ db }) => {
 const CountryByNameSourceLayer: React.FC<{
   result: any;
 }> = ({ result }) => {
-  const map = useMap();
+  const { countryByNameMap: map } = useMap();
 
   useEffect(() => {
-    // 地図を中心に移動する
+    // 地図をresultの中心に移動する
     if (result && map) {
       const [minLng, minLat, maxLng, maxLat] = turf.bbox(result);
       const bounds = [
         [minLng, minLat],
         [maxLng, maxLat],
       ] as LngLatBoundsLike;
-      map.current?.fitBounds(bounds, {
+      map?.fitBounds(bounds, {
         padding: 10,
         duration: 500,
       });
@@ -161,7 +161,7 @@ const CountryByName: React.FC<{ db: duckdb.AsyncDuckDB }> = ({ db }) => {
 
   useEffect(() => {
     // 国名を指定して国の情報を取得するクエリ
-    // AsGeoJson関数を使ってGeoJSON形式で取得
+    // ST_AsGeoJson関数を使ってGeoJSON形式で取得
     setQuery(`
       LOAD json;
       LOAD spatial;
@@ -193,7 +193,9 @@ const CountryByName: React.FC<{ db: duckdb.AsyncDuckDB }> = ({ db }) => {
 
   return (
     <div>
-      <h2>Country by name (Using ST_AsGeoJSON and render result on the map)</h2>
+      <h2>
+        Country by name (Using ST_AsGeoJSON and render one result on the map)
+      </h2>
       <input
         type="text"
         value={countryName}
@@ -213,6 +215,117 @@ const CountryByName: React.FC<{ db: duckdb.AsyncDuckDB }> = ({ db }) => {
           mapStyle="https://tile.openstreetmap.jp/styles/osm-bright/style.json"
         >
           {result && <CountryByNameSourceLayer result={result} />}
+        </Map>
+      </MapProvider>
+    </div>
+  );
+};
+
+const CountryByPopulationSourceLayer: React.FC<{
+  results: any;
+}> = ({ results }) => {
+  const { countryByPopulationMap: map } = useMap();
+
+  useEffect(() => {
+    if (results && map) {
+      const [minLng, minLat, maxLng, maxLat] = turf.bbox({
+        type: "FeatureCollection",
+        features: results,
+      });
+      const bounds = [
+        [minLng, minLat],
+        [maxLng, maxLat],
+      ] as LngLatBoundsLike;
+      map?.fitBounds(bounds, {
+        padding: 10,
+        duration: 500,
+      });
+    }
+  }, [results, map]);
+
+  return (
+    <>
+      {results.map((result: any, i: number) => (
+        <Source key={i} type="geojson" data={result}>
+          <Layer
+            id={`result-${i}`}
+            type="fill"
+            paint={{ "fill-color": "red", "fill-opacity": 0.5 }}
+          />
+        </Source>
+      ))}
+    </>
+  );
+};
+
+const CountryByPopulation: React.FC<{ db: duckdb.AsyncDuckDB }> = ({ db }) => {
+  const [minPopulation, setMinPopulation] = useState<number>(20000000);
+  const [results, setResults] = useState<any[] | null>(null);
+  const [query, setQuery] = useState<string | null>(null);
+
+  useEffect(() => {
+    // 人口が指定値以上の国の情報を取得するクエリ
+    // ST_AsGeoJson関数を使ってGeoJSON形式で取得
+    setQuery(`
+      LOAD json;
+      LOAD spatial;
+      SELECT name as name, POP_EST as population, ST_AsGeoJSON(geom) as geom FROM countries WHERE POP_EST > ${minPopulation};
+    `);
+  }, [minPopulation]);
+
+  useEffect(() => {
+    const fetch = async () => {
+      if (!query) return;
+      const conn = await db.connect();
+      const results: Table = await conn.query(query);
+      const resultRows: StructRowProxy<any>[] = results.toArray();
+      if (resultRows.length === 0) {
+        setResults(null);
+        return;
+      }
+      // geojsonにする
+      const resultGeoJson = resultRows.map((row) => ({
+        type: "Feature",
+        properties: { name: row.name, population: row.population },
+        geometry: JSON.parse(row.geom),
+      }));
+      setResults(resultGeoJson);
+      await conn.close();
+    };
+    fetch();
+  }, [db, query]);
+
+  return (
+    <div>
+      <h2>
+        Country by population (Using ST_AsGeoJSON and render multiple results on
+        the map)
+      </h2>
+      <input
+        style={{
+          width: "60%",
+        }}
+        type="range"
+        min={1}
+        max={1600000000}
+        step={1000000}
+        value={minPopulation}
+        onChange={(e) => setMinPopulation(Number(e.target.value))}
+      />
+      <pre>{query}</pre>
+      {!results && <p>Loading...</p>}
+      <MapProvider>
+        <Map
+          id="countryByPopulationMap"
+          style={{ width: 600, height: 400 }}
+          initialViewState={{
+            latitude: 0,
+            longitude: 0,
+            zoom: 1,
+          }}
+          mapStyle="https://tile.openstreetmap.jp/styles/osm-bright/style.json"
+        >
+          {results && <CountryByPopulationSourceLayer results={results} />}
         </Map>
       </MapProvider>
     </div>
@@ -277,6 +390,7 @@ function App() {
               <NumberOfCountries db={myDuckDB} />
               <LargestCountries db={myDuckDB} />
               <CountryByName db={myDuckDB} />
+              <CountryByPopulation db={myDuckDB} />
             </>
           ) : (
             <p>Data loading...</p>
